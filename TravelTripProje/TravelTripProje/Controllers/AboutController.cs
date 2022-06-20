87610cf12451b85f228e194937c8b403074604f6
tayfunkayhantk <tayfunkayhantk@gmail.com>
2022-06-20 18:46:35 +0300

    Mvc C# Asp.Net TravelTrip
    Mvc C# Asp.Net ile Yazdığım Seyahat Sitesi

---
+++ TravelTripProje/TravelTripProje/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AboutController : Controller
+    {
+        Context c = new Context();
+        
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
